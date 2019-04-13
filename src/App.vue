3c4ae63bc5d8bb2a5b28d5f4ebf47d7f9bc03e0c
ArthurWebWare <arthur.webware@gmail.com>
2019-04-13 03:56:16 +0300

    add store.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">page-header</span><span class="nt">&gt;</span>
+    <span class="nt">&lt;h1&gt;</span>ToDo List<span class="nt">&lt;/h1&gt;</span>
+    <span class="nt">&lt;p</span> <span class="na">class=</span><span class="s">"lead"</span><span class="nt">&gt;</span>An expample of business sheet <span class="nt">&lt;/p&gt;</span>
+    <span class="nt">&lt;/div&gt;</span>
+
+    <span class="nt">&lt;router-view/&gt;</span>
     <router-view/>
   </div>
 </template>
@@ -11,5 +17,19 @@ export default {
 </script>
 
 <style>
+  .page-header h1 {
+    font-weight: 300;
+  }
 
+  .card {
+    margin-bottom: 10px;
+  }
+
+  .card:last-child {
+    margin-bottom: 25px;
+  }
+
+  .card-block {
+    padding: 15px;
+  }
 </style>
